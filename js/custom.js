471259b5b7a7b00bbdc21bd90f72956589d3cc94
Dzmitry Yarmoshkin <spanb4@gmail.com>
2021-10-19 20:29:12 +0300

    feat: add tab functional

--- js/custom.js
+++ js/custom.js
@@ -19,6 +19,8 @@ function toggleTab(selectedNav, targetId) {
   navEls.forEach(function (navEl) {
     if (navEl.id == selectedNav) {
       navEl.classList.add('is-active');
+      navEl.setAttribute('aria-selected', 'true');
+      navEl.setAttribute('tabindex', '0');
     } else {
       if (navEl.classList.contains('is-active')) {
         navEl.classList.remove('is-active');
@@ -40,3 +42,49 @@ function toggleTab(selectedNav, targetId) {
 function showNewTabMessage() {
   alert('will open in a new tab');
 }
+
+let tabs = document.querySelectorAll('.tabs ul li');
+const LEFT_ARROW = 'ArrowLeft';
+const RIGHT_ARROW = 'ArrowRight';
+const ENTER = 'Enter';
+const SPACE = 'Space';
+const HOME = 'Home';
+const END = 'End';
+const IS_ACTIVE = 'is-active';
+
+function deactivateTabs() {
+  tabs.forEach((tab) => {
+    tab.setAttribute('tabindex', '-1');
+    tab.setAttribute('aria-selected', 'false');
+  });
+}
+
+function activateTab(target, direction) {
+  if (direction === LEFT_ARROW) {
+    target.previousSibling.previousSibling.focus();
+  } else if (direction === RIGHT_ARROW) {
+    target.nextSibling.nextSibling.focus();
+  } else {
+    target.focus();
+  }
+}
+
+tabs.forEach((tab) => {
+  tab.addEventListener('keydown', (e) => {
+    if (e.code === RIGHT_ARROW && e.target.closest('li').nextSibling.nextSibling) {
+      activateTab(e.target.closest('li'), RIGHT_ARROW);
+    } else if (e.code === LEFT_ARROW && e.target.closest('li').previousSibling.previousSibling) {
+      activateTab(e.target.closest('li'), LEFT_ARROW);
+    } else if (e.code === ENTER || e.code === SPACE) {
+      e.preventDefault();
+      deactivateTabs();
+      e.target.click();
+    } else if (e.code === HOME) {
+      e.preventDefault();
+      activateTab(tabs[0]);
+    } else if (e.code === END) {
+      e.preventDefault();
+      activateTab(tabs[tabs.length - 1]);
+    }
+  });
+});
